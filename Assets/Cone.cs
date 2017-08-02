5cd0a2287ed78c94ab9172180e4cc77a47262d22
NinjaDanceMat <cathcart100@googlemail.com>
2017-08-02 18:19:14 +0100

    Cone of Vision. Constant rumble needs fixed

---
+++ Assets/Cone.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cone : MonoBehaviour
+{
+    public MeshFilter meshRender;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<Vector3> vetices = new List<Vector3>();
+
+        float angle = -50;
+        int RaysToShoot = 100;
+
+        for (int i = 0; i < RaysToShoot; i++)
+        {
+            Vector3 direction = Quaternion.AngleAxis(angle, transform.up) * transform.forward;
+
+            angle += 100 / RaysToShoot;
+
+            Vector3 dir = new Vector3(direction.x, 0, direction.y);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(transform.position, dir, out hit, 20.1f))
+            {
+                vetices.Add(hit.point);
+            }
+            else
+            {
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vetices.ToArray();
+
+        meshRender.mesh = mesh;
+    }
+}
+
